1374e3482ff1e05ee2d9f54d8e27e3c8b14ef1ba
itsakmak <74183205+itsakmak@users.noreply.github.com>
2025-09-05 11:16:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires='>=3.12',
     install_requires=[ 
         'boto3>=1.36.7',
-        'envrio_logger @ git+ssh://git@github.com/Envrio-hub/envrio_logger.git@0.1.0'
+        'envrio_logger @ git+ssh://git@github.com/Envrio-hub/envrio_logger.git@0.1.1'
     ],
     classifiers=[  
         'Development Status :: 4 - Beta',
